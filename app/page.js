12827a291b3ecb28e55005266ebf4bd9b296e241
jhunter2026 <joeygrimmer.production@gmail.com>
2026-08-14 13:45:56 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export const dynamic = 'force-dynamic';
 const FEEDS = [
   { url: 'https://feeds.npr.org/1001/rss.xml', source: 'NPR' },
   { url: 'https://gothamist.com/feed', source: 'Gothamist' },
-  { url: 'https://laist.com/rss/latest-news', source: 'LAist' },
+  { url: 'https://laist.com/index.atom', source: 'LAist' },
   { url: 'https://www.themeateater.com/feed', source: 'MeatEater' },
   { url: 'https://www.thefp.com/feed', source: 'The Free Press' },
 ];
